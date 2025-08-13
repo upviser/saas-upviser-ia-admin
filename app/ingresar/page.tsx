f919e8c5000278359ebd0ccb8533a91e16da87af
upvisor <admin@upvisor.cl>
2025-08-13 16:56:01 -0400

    actualization general

--- app/ingresar/page.tsx
+++ app/ingresar/page.tsx
@@ -29,7 +29,9 @@ export default function Page () {
   useEffect(() => {
     const getPlan = () => {
       const planParam = searchParams.get("plan")
-      setLoginData({ ...loginData, plan: planParam ? planParam : '', imagesAI: planParam ? planParam === 'Esencial' ? 40 : planParam === 'Avanzado' ? 80 : planParam === 'Profesional' ? 120 : 0 : 0, videosAI: planParam ? planParam === 'Esencial' ? 20 : planParam === 'Avanzado' ? 40 : planParam === 'Profesional' ? 80 : 0 : 0, conversationsAI: planParam ? planParam === 'Esencial' ? 500 : planParam === 'Avanzado' ? 1000 : planParam === 'Profesional' ? 2000 : 0 : 0, emails: planParam ? planParam === 'Esencial' ? 100 : planParam === 'Avanzado' ? 200 : planParam === 'Profesional' ? 300 : 0 : 0 })
+      const now = new Date()
+      const days = new Date(now.getFullYear(), now.getMonth() + 1, 0).getDate()
+      setLoginData({ ...loginData, plan: planParam ? planParam : '', imagesAI: planParam ? planParam === 'Esencial' ? 40 : planParam === 'Avanzado' ? 80 : planParam === 'Profesional' ? 120 : 0 : 0, videosAI: planParam ? planParam === 'Esencial' ? 20 : planParam === 'Avanzado' ? 40 : planParam === 'Profesional' ? 80 : 0 : 0, conversationsAI: planParam ? planParam === 'Esencial' ? 500 : planParam === 'Avanzado' ? 1000 : planParam === 'Profesional' ? 2000 : 0 : 0, emails: planParam ? planParam === 'Esencial' ? 100 * days : planParam === 'Avanzado' ? 200 * days : planParam === 'Profesional' ? 300 * days : 0 : 0 })
     }
 
     getPlan()
