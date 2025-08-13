3d18dfea9c1774124228cb1337ba6fc7e8d1af19
upvisor <admin@upvisor.cl>
2025-08-13 17:33:57 -0400

    actualization general

--- app/page.tsx
+++ app/page.tsx
@@ -14,24 +14,32 @@ export default function Page () {
           <h3 className='font-medium'>Tareas iniciales</h3>
           <Table th={['Tarea', '']}>
             <tr>
-              <td className="p-2">Completar datos del negocio</td>
+              <td className="p-2">Completa los datos del negocio</td>
               <td className="p-2"><ButtonLink href={"/configuracion"}>Completar</ButtonLink></td>
             </tr>
             <tr>
-              <td className="p-2">Elegir color y estilo del sitio web</td>
-              <td className="p-2"><ButtonLink href={"/diseno?page=Estilo"}>Completar</ButtonLink></td>
-            </tr>
-            <tr>
-              <td className="p-2">Agregar productos</td>
+              <td className="p-2">Agrega tus productos</td>
               <td className="p-2"><ButtonLink href={"/productos"}>Completar</ButtonLink></td>
             </tr>
             <tr>
-              <td className="p-2">Agregar servicios</td>
+              <td className="p-2">Agrega tus servicios</td>
               <td className="p-2"><ButtonLink href={"/servicios"}>Completar</ButtonLink></td>
             </tr>
             <tr>
-              <td className="p-2">Agregar paginas y diseñar cada una</td>
+              <td className="p-2">Elije el color y estilo del sitio web</td>
+              <td className="p-2"><ButtonLink href={"/diseno?page=Estilo"}>Completar</ButtonLink></td>
+            </tr>
+            <tr>
+              <td className="p-2">Agrega paginas y diseña cada una</td>
               <td className="p-2"><ButtonLink href={"/diseno"}>Completar</ButtonLink></td>
+            </tr>
+            <tr>
+              <td className="p-2">Agrega los metodos de pago</td>
+              <td className="p-2"><ButtonLink href={"/configuracion/pasarela-de-pago"}>Completar</ButtonLink></td>
+            </tr>
+            <tr>
+              <td className="p-2">Agrega el metodo de envío</td>
+              <td className="p-2"><ButtonLink href={"/configuracion/envios"}>Completar</ButtonLink></td>
             </tr>
           </Table>
         </div>
