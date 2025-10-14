ec374dad99acbbe3a623eec3373101072b46c321
upvisor <admin@upvisor.cl>
2025-10-14 12:57:00 -0300

    actualization general

--- app/registro/page.tsx
+++ app/registro/page.tsx
@@ -87,7 +87,7 @@ export default function Page () {
 
         await apiClient.post('/create-default-design', { tenantId })
 
-        await apiClient.post('/domain', { domain: `${loginData.subdomain}.upviser.cl`, tenantId: tenantId })
+        await apiClient.put('/domain', { domain: `${loginData.subdomain}.upviser.cl`, tenantId: tenantId })
         
         const res = await signIn('credentials', {
           email: loginData.email,
